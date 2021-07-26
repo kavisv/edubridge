67e7465c4f1476473837a21d0edb6bc5e56b6c6c
svkavi <svkavi1999@gmail.com>
2021-07-26 09:41:04 +0530

    project

---
+++ java/WhileLoop.java
@@ -0,0 +1,9 @@
+class MyFirstJavaClass{
+    public static void main(String[] args){
+        int num1=0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}       
